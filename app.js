59f36a1b46e041019fcc688d497623cbf5256ce5
Gardo Kirsipuu <gardo1999@gmail.com>
2021-10-20 20:55:44 +0300

    Removing books + localstorage complete

--- app.js
+++ app.js
@@ -5,15 +5,31 @@ const form = document.querySelector('form')
 const bookTitleInput = document.querySelector('#book-title');
 const bookAuthorInput = document.querySelector('#book-author');
 const bookISBNInput = document.querySelector('#book-isbn');
+const bookList = document.querySelector('tbody.table-list');
+
+document.addEventListener('DOMContentLoaded', getBooks)
 
 form.addEventListener('submit', addBook);
-console.log(document.querySelector('tbody.table-list'));
+bookList.addEventListener('click', deleteBook);
 
 function addBook(e) {
 	const bookTitle = bookTitleInput.value;
 	const bookAuthor = bookAuthorInput.value;
 	const bookISBN = bookISBNInput.value;
-	ui.addBook(bookTitle, bookAuthor, bookISBN);
-	console.log('clicked')
+	let bookNew = new book(bookTitle, bookAuthor, bookISBN);
+	ui.addBook(bookNew);
+	ls.addBook(bookNew);
 	e.preventDefault();
 }
+
+function deleteBook(e){
+	let book = e.target.parentElement.firstChild;
+	ui.deleteBook(book);
+	book = book.parentElement.previousElementSibling.textContent;
+	ls.deleteBook(book);
+}
+
+function getBooks() {
+		books = ls.getData();
+		books.forEach(ui.getBooks.bind(books));
+	}
